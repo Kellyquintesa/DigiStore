c8ad09b55269669526b3edb776e4c2ec638f9098
kellyquintesa <kellyquintesa11@gmail.com>
2022-06-15 19:57:55 +0700

    add files on pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,11 @@ import "../styles/homepage.css";
 import "../styles/utilities.css";
 import "../styles/detail.css";
 import "../styles/checkout.css";
+import "../styles/complete-checkout.css";
+import "../styles/sign-in.css";
+import "../styles/sign-up.css";
+import "../styles/sign-up-success.css";
+import "../styles/page-not-found.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
