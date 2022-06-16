100743a03a642408d8e2f34ffad7e5d05fff2522
kellyquintesa <kellyquintesa11@gmail.com>
2022-06-16 13:40:18 +0700

    add navbar function

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+    },
+  },
+  plugins: [],
+};
