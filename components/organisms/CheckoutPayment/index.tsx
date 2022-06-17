5d455c6747ff00de5ad5a04a6ec75e19f8a156df
kellyquintesa <kellyquintesa11@gmail.com>
2022-06-17 16:37:02 +0700

    add components in organisms folder

---
+++ components/organisms/CheckoutPayment/index.tsx
@@ -0,0 +1,13 @@
+function CheckoutPayment() {
+  return (
+    <>
+      <label className="checkbox-label text-lg color-palette-1">
+        I have transferred the money
+        <input type="checkbox" />
+        <span className="checkmark"></span>
+      </label>
+    </>
+  );
+}
+
+export default CheckoutPayment;
